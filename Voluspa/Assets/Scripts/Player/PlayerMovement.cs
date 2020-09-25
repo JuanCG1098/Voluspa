f1635c02c523994955014aa43fb7d806fab7d01f
JuanCG1098 <JuanCGallardo1098@gmail.com>
2020-09-25 08:23:40 -0300

    Organizacion de scripts

---
+++ Voluspa/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    //bool croach = false;
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        float inputX = Input.GetAxis("Horizontal");
+
+        // Swap direction of sprite depending on walk direction
+        if (inputX > 0)
+            transform.localScale = new Vector3(-2.0f, 2.0f, 1.0f);
+        else if (inputX < 0)
+            transform.localScale = new Vector3(2.0f, 2.0f, 1.0f);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        //if (Input.GetButtonDown("Croach"))
+        //{
+        //    croach = true;
+        //}else if (Input.GetButtonUp("Croach"))
+        //{
+        //    croach = false;
+        //}
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
